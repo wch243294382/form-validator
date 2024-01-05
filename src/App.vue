06814e99a16a6a9855ecc030ad519375ee82acd9
吴昌昊 <243294382@qq.com>
2024-01-05 11:05:50 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,103 @@ console.log(a)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <form id="form" class="form">
+      <h2>Register With Us</h2>
+      <div class="form-control">
+        <label for="username">Username</label>
+        <input type="text" id="username" placeholder="Enter username" />
+        <small>Error Message</small>
+      </div>
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="text" id="email" placeholder="Enter email" />
+        <small>Error Message</small>
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Enter password" />
+        <small>Error Message</small>
+      </div>
+      <div class="form-control">
+        <label for="password2">Confirm Password</label>
+        <input type="password" id="password2" placeholder="Enter password2" />
+        <small>Error Message</small>
+      </div>
+      <button>Submit</button>
+    </form>
   </main>
 </template>
 
 <style scoped lang="scss">
-header {
-  line-height: 1.5;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  background-color: #f5f5f5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.form {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  width: 100%;
+  label {
+    display: block;
+    margin-bottom: 5px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  h2 {
+    text-align: center;
+    margin-bottom: 20px;
   }
+  .form-control {
+    margin-bottom: 10px;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    input {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      font-size: 16px;
+
+      &:focus {
+        outline: 0;
+        border-color: black;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+      }
+      &.success {
+        border-color: var(--success-color);
+      }
+
+      &.error {
+        border-color: var(--error-color);
+      }
+    }
+    small {
+      color: var(--error-color);
+      // color: red;
+      display: block;
+      visibility: hidden;
+      margin-top: 5px;
+
+      &.error {
+        visibility: visible;
+      }
+    }
+  }
+  button {
+    width: 100%;
+    padding: 10px;
+    background-color: #5482c5;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
   }
 }
 </style>
